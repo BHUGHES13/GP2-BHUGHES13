6a874c5c4ae04cec7f47419a3b012db51c07f6ad
BHUGHES13 <bryan_hughes@live.com>
2014-09-30 09:50:15 +0100

    Initial files added

---
+++ GPUlab/main.cpp
@@ -0,0 +1,18 @@
+//Header
+#include <iostream>
+
+//Glabal Variables
+
+//Glabal Functions
+void InitWindow(int width, int hieght, bool fullscreen){
+
+}
+
+void cleanup(){
+}
+
+//Main method - entry point
+
+int main(int argc, char*arg){
+	return 0;
+}
